df03385e7cd0d210833d2279727146a221963324
Prakash Salapu <bhanuprakash8305@gmail.com>
2026-06-01 19:11:35 +0530

    add RemoveDuplicates in Arrays -> Easy

---
+++ src/p3_arrays/Easy/RemoveDuplicates.java
@@ -0,0 +1,31 @@
+package p3_arrays.Easy;
+
+//  LeetCode: 26. Remove Duplicates from Sorted Array
+//  TC: O() | SC: O()
+
+public class RemoveDuplicates {
+
+    public static int removeDuplicates(int[] nums){
+
+        int i = 0;
+        for(int j =1;j<nums.length; j++){
+           if(nums[i]!=nums[j]) {
+               nums[i + 1] = nums[j];
+               i++;
+           }
+        }
+        return i+1;
+
+
+    }
+    public static void main(String[] args) {
+        int[] nums = {-2, 2, 4, 4, 4, 4, 5, 5};
+        int result = removeDuplicates(nums);
+
+        for(int i=0; i<result; i++){
+            System.out.print(nums[i] + " ");
+        }
+
+    }
+
+}
