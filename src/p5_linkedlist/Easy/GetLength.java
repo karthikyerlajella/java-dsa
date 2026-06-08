3640e5079f9c0a0d4fc754661c2b11545c670150
Prakash Salapu <bhanuprakash8305@gmail.com>
2026-06-08 13:41:15 +0530

    add LengthOfLL in LinkedList->Easy

---
+++ src/p5_linkedlist/Easy/GetLength.java
@@ -0,0 +1,35 @@
+package p5_linkedlist.Easy;
+
+class Node{
+    int data;
+    Node next;
+
+    Node(int data, Node next){
+        this.data = data;
+        this.next = next;
+    }
+}
+
+public class GetLength {
+
+    public static int getLength(Node head) {
+
+        int count = 0;
+        Node temp = head;
+        while(temp != null){
+            count++;
+            temp = temp.next;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+
+        Node head = new Node(10, new Node(20, new Node(30, new Node(40, null))));
+
+        int count = getLength(head);
+        System.out.println("Length of the LinkedList : " + count);
+
+
+    }
+}
