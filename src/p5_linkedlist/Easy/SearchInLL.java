a5f1981ea53b2fc6dac8ec4349717ecbbe7d9130
Prakash Salapu <bhanuprakash8305@gmail.com>
2026-06-08 14:04:20 +0530

    add SearchInLL in LinkedList->Easy

--- src/p5_linkedlist/Easy/SearchInLL.java
+++ src/p5_linkedlist/Easy/SearchInLL.java
@@ -1,13 +1,12 @@
 package p5_linkedlist.Easy;
-
 import p5_linkedlist.Node;
 
 public class SearchInLL {
 
-    public static int getLength(Node head, int val) {
+    public static int searchKey(Node head, int key) {
         Node temp = head;
         while(temp != null){
-            if(temp.data == val) return 1;
+            if(temp.data == key) return 1;
             temp = temp.next;
         }
         return 0;
@@ -16,10 +15,10 @@ public class SearchInLL {
     public static void main(String[] args) {
 
         Node head = new Node(10, new Node(20, new Node(30, new Node(40, null))));
-        int val = 140;
-        int result = getLength(head,val);
-        if(result == 1) System.out.println(val + " is Found");
-        else System.out.println(val + " is Not Found");
+        int key = 140;
+        int result = searchKey(head,key);
+        if(result == 1) System.out.println(key + " is Found");
+        else System.out.println(key + " is Not Found");
 
     }
 }
