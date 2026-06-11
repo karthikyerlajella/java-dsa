6ac4d9ccc7a6496f6a9578ca421be1af6caea548
Prakash Salapu <bhanuprakash8305@gmail.com>
2026-06-11 12:05:37 +0530

    add InsertNode to LL->Easy

--- src/p5_linkedlist/Easy/InsertNode.java
+++ src/p5_linkedlist/Easy/InsertNode.java
@@ -11,6 +11,11 @@ public class InsertNode {
 
     // Insert at Tail
     public static Node insertAtTail(Node head, int val){
+
+        if(head == null){
+            return new Node(val,null);
+        }
+
         Node curr = new Node(val, null);
         Node temp = head;
 
@@ -22,9 +27,56 @@ public class InsertNode {
     }
 
     // Insert at Position
-    public static Node insertAtPosition(Node head, int val){
-        Node temp = new Node(val, head);
-        return temp;
+    public static Node insertAtPosition(Node head, int pos, int val){
+
+        if(pos < 1){
+            System.out.println("Enter Valid Position..");
+            return head;
+        }
+
+        if(pos == 1){
+            return new Node(val,head);
+        }
+
+        int count  = 0;
+        Node temp = head;
+
+        while(temp != null){
+            count++;
+            if(count == (pos-1)){
+                Node newNode = new Node(val, null);
+                newNode.next = temp.next;
+                temp.next = newNode;
+                break;
+            }
+            temp = temp.next;
+        }
+
+       return head;
+    }
+
+    // Insert before element
+    public static Node insertBeforeElement(Node head, int val, int element){
+
+        if(head == null) return null;
+
+        if(head.data == element){
+            return new Node(val,head);
+        }
+
+        Node temp = head;
+
+        while(temp.next != null){
+            if(temp.next.data == element){
+                Node newNode = new Node(val, null);
+                newNode.next = temp.next;
+                temp.next = newNode;
+                break;
+            }
+            temp = temp.next;
+        }
+
+       return head;
     }
 
 
@@ -33,7 +85,8 @@ public class InsertNode {
 
 //        Node temp = insertAtHead(head, 100);
 //        Node temp = insertAtTail(head, 4);
-        Node temp = insertAtPosition(head, 4);
+//        Node temp = insertAtPosition(head, 3, 5);
+        Node temp = insertBeforeElement(head, 5, 20);
 
         while(temp != null){
             System.out.print(temp.data + " -> ");
