5493b97daa0b95076b6fe411cf84c46617a3ffea
Prakash Salapu <bhanuprakash8305@gmail.com>
2026-06-11 14:19:26 +0530

    add FindMiddleNode to LL->Easy

---
+++ src/p5_linkedlist/Easy/FindMiddleNode.java
@@ -0,0 +1,32 @@
+package p5_linkedlist.Easy;
+import p5_linkedlist.Node;
+
+//  LeetCode: 876. Middle of the Linked List
+//  TC: O(n) | SC: O(1)
+
+public class FindMiddleNode {
+
+    public static Node middleNode(Node head){
+        Node slow = head;
+        Node fast = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+    public static void main(String[] args) {
+        Node head = new Node(1, new Node(1, new Node(3, new Node(4, new Node(5, null)))));
+
+        Node temp = middleNode(head);
+
+        while(temp != null){
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+}
+
