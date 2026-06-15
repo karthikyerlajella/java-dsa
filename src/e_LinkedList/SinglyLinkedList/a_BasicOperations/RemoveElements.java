b778a5cb05ff7bbf17c23f07ab3c56eee269a586
Prakash Salapu <bhanuprakash8305@gmail.com>
2026-06-15 22:46:48 +0530

    add LeetCode: 203. Remove Linked List Elements

---
+++ src/e_LinkedList/SinglyLinkedList/a_BasicOperations/RemoveElements.java
@@ -0,0 +1,42 @@
+package e_LinkedList.SinglyLinkedList.a_BasicOperations;
+import e_LinkedList.SinglyLinkedList.ListNode;
+
+/*
+ LeetCode: 203. Remove Linked List Elements
+ Difficulty: Easy
+ TC: O(N) | SC: O(1)
+*/
+
+public class RemoveElements {
+    public static ListNode removeElements(ListNode head, int val) {
+
+        while (head != null && head.data == val) {
+            head = head.next;
+        }
+
+        ListNode curr = head;
+        while (curr != null && curr.next != null) {
+            if (curr.next.data == val) {
+                curr.next = curr.next.next;
+            } else {
+                curr = curr.next;
+            }
+        }
+
+        return head;
+
+    }
+
+    public static void main(String[] args) {
+
+        ListNode head = new ListNode(10, new ListNode(20, new ListNode(10, new ListNode(40, null))));
+
+        ListNode temp = removeElements(head, 10);
+
+        while(temp != null){
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+}
