ebc9023e878195df5839e1c00916efa48896c7dd
Prakash Salapu <bhanuprakash8305@gmail.com>
2026-06-18 18:47:31 +0530

    Add LeetCode: 24. Swap Nodes in Pairs

---
+++ src/e_LinkedList/SinglyLinkedList/c_Reversal/SwapNodesinPairs.java
@@ -0,0 +1,55 @@
+package e_LinkedList.SinglyLinkedList.c_Reversal;
+import e_LinkedList.SinglyLinkedList.ListNode;
+
+/*
+    LeetCode: 24. Swap Nodes in Pairs
+    Difficulty: Medium
+    TC: O(N) | SC: O(1)
+    Pattern : DummyNode + Reversal
+*/
+
+public class SwapNodesinPairs {
+
+    public static ListNode swapPairs(ListNode head) {
+
+        if(head == null || head.next == null) return head;
+
+        ListNode dummy = new ListNode(0,head);
+        ListNode prev = dummy;
+
+        while(prev.next != null && prev.next.next != null){
+
+            ListNode first = prev.next;
+            ListNode second = first.next;
+
+            first.next = second.next;
+            second.next = first;
+            prev.next = second;
+
+            prev = first;
+
+        }
+
+        return dummy.next;
+
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(50, new ListNode(40, new ListNode(30, new ListNode(20, new ListNode(10,null)))));
+
+        System.out.print("Before : ");
+        for(ListNode curr=head; curr != null; curr = curr.next){
+            System.out.print(curr.data  + " -> ");
+        }
+        System.out.print("null");
+
+        System.out.println();
+        ListNode temp = swapPairs(head);
+
+        System.out.print("After : ");
+        for(ListNode curr=temp; curr != null; curr = curr.next){
+            System.out.print(curr.data  + " -> ");
+        }
+        System.out.print("null");
+    }
+}
