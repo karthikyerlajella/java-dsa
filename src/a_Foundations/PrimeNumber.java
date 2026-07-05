1fcfa6b3c32f4e59c8ddcb95447568f0858f8f4d
iamkarthik99 <karthik.yerlajella99@gmail.com>
2026-07-05 22:25:59 +0530

    PrimeNumber.java

---
+++ src/a_Foundations/PrimeNumber.java
@@ -0,0 +1,28 @@
+package a_Foundatoions;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Number:");
+        int n = sc.nextInt();
+        boolean isPrime = true;
+        if (n<=1){
+            isPrime=false;
+        }else {
+            for (int i=2;i*i<=n;i++){
+                if (n%i==0){
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+        if (isPrime)
+            System.out.println("Prime Number");
+        else
+            System.out.println("Not a Prime Number");
+
+        sc.close();
+    }
+}
